d278f039d5d963ae2f61f528c3478b76b5ee778b
terryfournier <terryfournier2@gmail.com>
2026-01-14 16:58:46 +0100

    Switch to rider programming
    Setup of rider with unreal complete
    First step for the Hivemind switch NPC to CPP

---
+++ HideFromMonster/Source/HideFromMonster/ActorRenamed.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ActorRenamed.generated.h"
+
+UCLASS()
+class HIDEFROMMONSTER_API AActorRenamed : public AActor
+{
+	GENERATED_BODY()
+	//Oui
+public:	
+	// Sets default values for this actor's properties
+	AActorRenamed();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
